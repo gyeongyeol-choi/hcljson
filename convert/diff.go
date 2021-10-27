fe263f396937e2e3297613bca9d1089dcda2651b
gyeongyeol-choi <gyeongyeol_choi@tmax.co.kr>
2021-10-27 10:37:54 +0000

    [mod] 1. HCL 비교함수 - 리소스 블록의 시작점, 끝점 반환 오류 수정 2. 리소스 블록 내 Exclusive Comment에 대한 처리 로직 추가 3. 주석 추가 시 정렬 로직 구현

--- convert/diff.go
+++ convert/diff.go
@@ -32,43 +32,72 @@ func HclToHcl(origin string, convert string) string {
 		if flag {
 			object += tmp + "\n"
 
-			if tmp == "}" {
+			if strings.HasPrefix(tmp, "}") {
 				flag = false
 				object = strings.Replace(object, "\n\n", "\n", -1)
+
 				m[key] = object
 				object = ""
 			}
 		}
+
 	}
 
 	flag = false
 	object = ""
+	cnt := 0
+	tmp_cmt := ""
 
 	var comment_map map[string]string
 	comment_map = make(map[string]string)
+
+	var post_map map[string]string
+	post_map = make(map[string]string)
 
 	o_slice := strings.Split(origin, "\n")
 
 	for _, tmp := range o_slice {
 		tmp = strings.TrimSpace(tmp)
 
-		if strings.HasPrefix(tmp, "provider") || strings.HasPrefix(tmp, "module") ||
+		if (strings.HasPrefix(tmp, "provider") || strings.HasPrefix(tmp, "module") ||
 			strings.HasPrefix(tmp, "variable") || strings.HasPrefix(tmp, "resource") ||
-			strings.HasPrefix(tmp, "data") {
+			strings.HasPrefix(tmp, "data")) && !flag {
 			flag = true
 
 			key = strings.Split(tmp, "{")[0]
 			key = strings.TrimRight(key, " ")
 			key = strings.Replace(key, "\"", "", -1)
 			key = strings.Replace(key, " ", "_", -1)
+
 		}
 
 		if !flag {
 			final_str += tmp + "\n"
 		} else {
+			if strings.Contains(tmp, "{") {
+				cnt++
+			}
+			if strings.Contains(tmp, "}") {
+				cnt--
+			}
+
 			object += tmp + "\n"
 
-			if strings.Contains(tmp, "#") {
+			if strings.Contains(tmp, "=") {
+				post_key := strings.Split(tmp, "=")[0]
+				post_key = strings.TrimSpace(post_key)
+
+				if !strings.Contains(post_key, "#") && tmp_cmt != "" {
+					post_map[post_key] = tmp_cmt
+					tmp_cmt = ""
+				}
+			}
+
+			if strings.HasPrefix(tmp, "#") && cnt == 1 {
+				tmp_cmt += tmp + "\n"
+			}
+
+			if strings.Contains(tmp, "#") && cnt == 1 {
 				slice := strings.SplitN(tmp, "#", 2)
 				cmt_val := "#" + slice[1]
 				cmt_key := ""
@@ -81,10 +110,14 @@ func HclToHcl(origin string, convert string) string {
 					cmt_key = strings.Split(slice[0], "=")[0]
 					cmt_key = strings.TrimSpace(cmt_key)
 				}
-				comment_map[cmt_key] = cmt_val
+
+				if cmt_key != "" {
+					comment_map[cmt_key] = cmt_val
+				}
 			}
 
-			if tmp == "}" {
+			if strings.HasPrefix(tmp, "}") && cnt == 0 {
+
 				flag = false
 				object = strings.Replace(object, "\n\n", "\n", -1)
 
@@ -103,14 +136,26 @@ func HclToHcl(origin string, convert string) string {
 							cmt_key = strings.Split(tmp, "=")[0]
 							cmt_key = strings.TrimSpace(cmt_key)
 						}
-						val2, exists2 := comment_map[cmt_key]
+
+						val2, exists2 := post_map[cmt_key]
 
 						if exists2 {
-							final_str += tmp + val2 + "\n"
-							cmt_key = ""
+							spaceCnt := countLeadingSpaces(tmp)
+
+							for i := 0; i < spaceCnt; i++ {
+								final_str += " "
+							}
+							final_str += val2
+						}
+						val3, exists3 := comment_map[cmt_key]
+
+						if exists3 {
+							final_str += tmp + val3 + "\n"
 						} else {
 							final_str += tmp + "\n"
 						}
+
+						cmt_key = ""
 					}
 				}
 				object = ""
@@ -119,4 +164,8 @@ func HclToHcl(origin string, convert string) string {
 		}
 	}
 	return final_str
+}
+
+func countLeadingSpaces(line string) int {
+	return len(line) - len(strings.TrimLeft(line, " "))
 }
