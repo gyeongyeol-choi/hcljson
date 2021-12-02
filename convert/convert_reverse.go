30d8b60a2ee537c1f200dd14d4ec698958cc56ff
gyeongyeol-choi <gyeongyeol_choi@tmax.co.kr>
2021-12-02 02:49:25 +0000

    [feat] map/object 분기 처리를 위한 regenJson 함수 프로토타입 구현

--- convert/convert_reverse.go
+++ convert/convert_reverse.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"strings"
 
 	jsonParser "github.com/tmax-cloud/hcljson/parser"
 	hclprinter "github.com/tmax-cloud/hcljson/printer"
@@ -15,6 +17,9 @@ func JsonToHcl(input []byte, typeSchemaStr string) []byte {
 
 	var typeSchema map[string]interface{}
 	json.Unmarshal([]byte(typeSchemaStr), &typeSchema)
+
+	// MEMO: json 재구성 함수 호출
+	input = regenJson(input)
 
 	bytes, err := convertJsonToHcl(input, typeSchema)
 	if err != nil {
@@ -34,4 +39,130 @@ func convertJsonToHcl(input []byte, typeSchema map[string]interface{}) ([]byte, 
 	}
 
 	return buf.Bytes(), nil
+}
+
+// MEMO : json 내 프로퍼티가 부모 프로퍼티 경로를 포함하도록 재구성 (map / object 구분 위함)
+func regenJson(input []byte) []byte {
+
+	var data map[string]interface{}
+	json.Unmarshal(input, &data)
+
+	for key, value := range data {
+		if strings.Contains(key, "**##**") {
+			continue
+		}
+
+		tmp := key
+		key = tmp + "**##**"
+
+		if reflect.TypeOf(value).Kind() == reflect.Map {
+			data1 := value.(map[string]interface{})
+
+			for key, value := range data1 {
+				if strings.Contains(key, "**##**") {
+					continue
+				}
+
+				tmp1 := key
+				key = tmp + "**##**" + tmp1
+
+				if reflect.TypeOf(value).Kind() == reflect.Map {
+					data2 := value.(map[string]interface{})
+
+					for key, value := range data2 {
+						if strings.Contains(key, "**##**") {
+							continue
+						}
+
+						tmp2 := key
+						key = tmp + "**##**" + tmp1 + "**##**" + key
+
+						if reflect.TypeOf(value).Kind() == reflect.Map {
+							data3 := value.(map[string]interface{})
+
+							for key, value := range data3 {
+								if strings.Contains(key, "**##**") {
+									continue
+								}
+								tmp3 := key
+								key = tmp + "**##**" + tmp1 + "**##**" + tmp2 + "**##**" + key
+
+								if reflect.TypeOf(value).Kind() == reflect.Map {
+									data4 := value.(map[string]interface{})
+									for key, value := range data4 {
+										if strings.Contains(key, "**##**") {
+											continue
+										}
+										tmp4 := key
+										key = tmp + "**##**" + tmp1 + "**##**" + tmp2 + "**##**" + tmp3 + "**##**" + key
+
+										if reflect.TypeOf(value).Kind() == reflect.Map {
+											data5 := value.(map[string]interface{})
+											for key, value := range data5 {
+												if strings.Contains(key, "**##**") {
+													continue
+												}
+												tmp5 := key
+												key = tmp + "**##**" + tmp1 + "**##**" + tmp2 + "**##**" + tmp3 + "**##**" + tmp4 + "**##**" + key
+
+												if reflect.TypeOf(value).Kind() == reflect.Map {
+													data6 := value.(map[string]interface{})
+													for key, value := range data6 {
+														if strings.Contains(key, "**##**") {
+															continue
+														}
+
+														tmp6 := key
+														key = tmp + "**##**" + tmp1 + "**##**" + tmp2 + "**##**" + tmp3 + "**##**" + tmp4 + "**##**" + tmp5 + "**##**" + key
+
+														data6[key] = value
+														delete(data6, tmp6)
+
+													}
+													data5[key] = data6
+
+												} else {
+													data5[key] = value
+												}
+												delete(data5, tmp5)
+											}
+											data4[key] = data5
+
+										} else {
+											data4[key] = value
+										}
+										delete(data4, tmp4)
+									}
+									data3[key] = data4
+								} else {
+									data3[key] = value
+								}
+								delete(data3, tmp3)
+							}
+							//data2[key] = data3
+							data2[tmp2] = data3
+						} else {
+							//data2[key] = value
+							data[tmp2] = value
+						}
+						//delete(data2, tmp2)
+					}
+					//data1[key] = data2
+					data1[tmp1] = data2
+				} else {
+					//data1[key] = value
+					data1[tmp1] = value
+				}
+				//delete(data1, tmp1)
+			}
+			//data[key] = data1
+			data[tmp] = data1
+		} else {
+			//data[key] = value
+			data[tmp] = value
+		}
+	}
+
+	output, _ := json.Marshal(data)
+	return output
 }
